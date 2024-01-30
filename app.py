96e76cdcbd45cdd9175c34c90f01ac31fc1d36f2
Sanket Suryawanshi <114396358+sankezzz@users.noreply.github.com>
2024-01-30 07:31:35 +0000

    modified

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/name")
+def hello():
+    return "hello"
+
+@app.route("/age")
+def age():
+    return "21"
+
+@app.route("/batch")
+def batch():
+    return "B3"
---
+++ templates/testfile.py
@@ -0,0 +1,6 @@
+import app
+def test_hello_world():
+    response =app .
+
+    
+                                   
